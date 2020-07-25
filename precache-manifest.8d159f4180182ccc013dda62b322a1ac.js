e7ffb1c36be47899de302c44c495485905afbdcf
rkbhambri <ramkrishan@redbaron.in>
2020-07-25 15:22:23 +0530

    Updates

---
+++ precache-manifest.8d159f4180182ccc013dda62b322a1ac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a866b5e3fb612fb2f7b5f4e4e47327",
+    "url": "/assignment-91social/index.html"
+  },
+  {
+    "revision": "03377c37863ae953d9d3",
+    "url": "/assignment-91social/static/css/4.bccf94e9.chunk.css"
+  },
+  {
+    "revision": "7c475bba023c23de0007",
+    "url": "/assignment-91social/static/css/5.0535e99d.chunk.css"
+  },
+  {
+    "revision": "5d61f71060eb843238f0",
+    "url": "/assignment-91social/static/css/main.15dd7940.chunk.css"
+  },
+  {
+    "revision": "d8ea3c2aa5e7cd937a11",
+    "url": "/assignment-91social/static/js/0.5845d19b.chunk.js"
+  },
+  {
+    "revision": "1aef7e2e17bb5e02f175",
+    "url": "/assignment-91social/static/js/3.f4c7cf54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/assignment-91social/static/js/3.f4c7cf54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03377c37863ae953d9d3",
+    "url": "/assignment-91social/static/js/4.656b1d3c.chunk.js"
+  },
+  {
+    "revision": "7c475bba023c23de0007",
+    "url": "/assignment-91social/static/js/5.76fd6d92.chunk.js"
+  },
+  {
+    "revision": "5d61f71060eb843238f0",
+    "url": "/assignment-91social/static/js/main.191e81f7.chunk.js"
+  },
+  {
+    "revision": "24a25fd9f2a668b00bdc",
+    "url": "/assignment-91social/static/js/runtime-main.4ddb7f29.js"
+  },
+  {
+    "revision": "6b73dbd3207d1191abd6687b1ebdc17c",
+    "url": "/assignment-91social/static/media/article.6b73dbd3.svg"
+  },
+  {
+    "revision": "425b91ecac94a153df413c0d100bc392",
+    "url": "/assignment-91social/static/media/wikipedia.425b91ec.jpg"
+  }
+]);
